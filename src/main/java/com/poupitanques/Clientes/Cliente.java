56e477a4bc0155214f8795e1c9d5d2237d614822
Agustin Castro <138733846+poupiTanques@users.noreply.github.com>
2024-10-01 20:33:01 -0300

    Añadir Cliente y Servidor TCP

---
+++ src/main/java/com/poupitanques/Clientes/Cliente.java
@@ -0,0 +1,34 @@
+package com.poupitanques.Clientes;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+        final String HOST = "127.0.0.1";
+        final int PUERTO = 5000;
+        DataInputStream in ;
+        DataOutputStream out;
+
+        try {
+            Socket sc = new Socket(HOST, PUERTO);
+
+            in = new DataInputStream(sc.getInputStream());
+            out = new DataOutputStream(sc.getOutputStream());
+
+            out.writeUTF("!Hola soy el el cliente");
+
+            String mensaje = in.readUTF();
+            System.out.println(mensaje);
+
+            sc.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
